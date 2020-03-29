3d718f0dc59b9e0fa4d1f62f2b6863bef159b2a3
Pavivin <pav-ivin@yandex.ru>
2020-03-30 01:15:12 +0300

    add comments

--- lib/products/product.dart
+++ lib/products/product.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/products/productInfo.dart';
-class Product extends StatefulWidget {
+class Product extends StatefulWidget { // класс, который может обновляться
   String name,
       brand,
       picture;
@@ -20,22 +20,26 @@ class Product extends StatefulWidget {
   class ProductState extends State<Product> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Hero(
+    return Card( // виджет, отвечающий за отображение информации
+      child: Hero( // виджет, отвечающий за анимацию
           tag: widget.id,
           child: Material(
-            child: InkWell(
+            child: InkWell( // область ответственная за касания пользователя
               onTap: () => Navigator.of(context).push(MaterialPageRoute(
                 builder: (context) => ProductInfo(
-                  name: widget.name, id:widget.id, picture: widget.picture,
-                  value: widget.value, brand: widget.brand,
-                  oldPrice: widget.oldPrice, price: widget.price,
+                  name: widget.name, // передача параметров в класс ProductInfo
+                  id:widget.id, // widget - класс Product
+                  picture: widget.picture,  
+                  value: widget.value, 
+                  brand: widget.brand,
+                  oldPrice: widget.oldPrice, 
+                  price: widget.price,
                 )
               )),
-              child: GridTile(
-                  footer: Container(
-                    color: Colors.white70,
-                    child: ListTile(
+              child: GridTile( // прокручиваемая таблица с виджетами
+                  footer: Container( // footer - виджет GridTile
+                    color: Colors.white70, // цвет с непрозрачностью 70
+                    child: ListTile( // список фиксированной длины, содержащий текст
                       leading: Text(
                         widget.name,
                         style: TextStyle(fontWeight: FontWeight.bold),
@@ -43,7 +47,7 @@ class Product extends StatefulWidget {
                       title: Text(
                         '$widget.price' + 'р',
                         style: TextStyle(
-                            color: Colors.red, fontWeight: FontWeight.w800),
+                            color: Colors.red, fontWeight: FontWeight.w800), // 
                       ),
                       subtitle: Text(
                         '$widget.oldPrice' + 'р',
