99832d43cfbb12229f08570c045464bf3c3f4eaa
Alexey-Sennikov <nesnaiker@yandex.ru>
2020-03-30 22:18:41 +0300

    more shoppingCart

---
+++ lib/registration/shoppingCart.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class ShoppingCart extends StatefulWidget{
+  @override
+  _ShoppingCart createState() => _ShoppingCart();
+}
+class _ShoppingCart extends State<ShoppingCart> {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
