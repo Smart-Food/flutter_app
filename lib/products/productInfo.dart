62c01280834d30270a35f356ee776322f67521ca
Alexey-Sennikov <55105158+Alexey-Sennikov@users.noreply.github.com>
2020-04-06 23:35:53 +0300

    Shopping cart (#17)
    * more shoppingCart
    
    * shoppingCart with Provider's bag

--- lib/products/productInfo.dart
+++ lib/products/productInfo.dart
@@ -1,5 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapp/registration/shoppingCart.dart';
+import 'package:flutterapp/registration/cart.dart';
+import 'package:provider/provider.dart';
+void main() => runApp(ProductInfoMain());
+class ProductInfoMain extends StatelessWidget{
+  @override
 
+  Widget build(BuildContext context) {
+    /*return MaterialApp(
+      //debugShowCheckedModeBanner: false, // стандарт от Google
+      home: ProductInfo(),
+    );*/
+
+     return ChangeNotifierProvider<Cart>(
+        create: (context){return Cart();},
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,  // стандарт от Google
+          home: Consumer<Cart> (
+            builder: (context,whatever,child) {return ProductInfo();},
+            //ProductInfo()
+          ),
+        )
+    );
+    /*return ChangeNotifierProvider<Cart>(
+      create: (context) => new Cart(),
+      child: Consumer<Cart>(
+        builder: (context,cart,_)
+        { return MaterialApp(
+        //debugShowCheckedModeBanner: false, // стандарт от Google
+        home: ProductInfo(),
+      );}
+      )
+    );*/
+
+
+    /*return ChangeNotifierProvider<Cart>.value(
+        value: Cart(),
+        child: MaterialApp(
+          //debugShowCheckedModeBanner: false,  // стандарт от Google
+          home: ProductInfo(),
+            //ProductInfo()
+          ),
+        );*/
+  }
+}
 class ProductInfo extends StatefulWidget {
   String name,
       brand,
@@ -8,17 +52,24 @@ class ProductInfo extends StatefulWidget {
       value,
       oldPrice;
   int id;
-  ProductInfo({
+  ProductInfo({ Key key,
     this.name, this.id, this.oldPrice, this.picture,
     this.value, this.price, this.brand
-});
+}):super(key:key);
   @override
   _ProductInfoState createState() => _ProductInfoState();
 }
 
 class _ProductInfoState extends State<ProductInfo> {
+
   @override
   Widget build(BuildContext context) {
+    Cart bloc = Provider.of<Cart>(context);
+    /*int totalCount = 0;
+    if (bloc.cart.length > 0) {
+      totalCount = bloc.cart.values.reduce((a, b) => a + b);
+    }*/
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0.1,
@@ -34,7 +85,10 @@ class _ProductInfoState extends State<ProductInfo> {
               icon: Icon(
                 Icons.shopping_cart,
                 color: Colors.white,
-              ), onPressed: () {})
+              ),
+              onPressed: () => Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => ShoppingCart()))
+          )
         ],
       ),
       body: ListView(
@@ -47,6 +101,33 @@ class _ProductInfoState extends State<ProductInfo> {
                 //child: Text('Цена: ' + widget.price.toString()),
               )),
             height: 300.0,
+          ),
+         /* GestureDetector(
+              onTap: () {
+                bloc.addToCart(widget);
+              },
+              child: Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    //image: AssetImage("assets/${index + 1}.jpg"),
+                    fit: BoxFit.fitWidth,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              )
+          ),*/
+          RaisedButton(
+            splashColor: Theme.of(context).primaryColor,
+            highlightColor: Theme.of(context).primaryColor,
+            color: Colors.white,
+            child: Text('Добавить в корзину'),
+            //style : TextStyle(fontWeight: FontWeight.bold, color: Theme.of(context).primaryColor, fontSize: 20),
+            onPressed: () {
+                //var bloc = Provider.of<Cart>(context, listen: false);
+                bloc.addToCart(widget);
+            },
           )
         ],
       ),
