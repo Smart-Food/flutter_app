ae32189f675320c24d62943c38ac5e2f23f80b26
Alexey-Sennikov <nesnaiker@yandex.ru>
2020-04-10 17:44:13 +0300

    upd cart

--- lib/products/productInfo.dart
+++ lib/products/productInfo.dart
@@ -1,60 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/registration/shoppingCart.dart';
-import 'package:flutterapp/registration/cart.dart';
-import 'package:provider/provider.dart';
-void main() => runApp(ProductInfoMain());
-class ProductInfoMain extends StatelessWidget{
-  @override
-
-  Widget build(BuildContext context) {
-    /*return MaterialApp(
-      //debugShowCheckedModeBanner: false, // стандарт от Google
-      home: ProductInfo(),
-    );*/
-
-     return ChangeNotifierProvider<Cart>(
-        create: (context){return Cart();},
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,  // стандарт от Google
-          home: Consumer<Cart> (
-            builder: (context,whatever,child) {return ProductInfo();},
-            //ProductInfo()
-          ),
-        )
-    );
-    /*return ChangeNotifierProvider<Cart>(
-      create: (context) => new Cart(),
-      child: Consumer<Cart>(
-        builder: (context,cart,_)
-        { return MaterialApp(
-        //debugShowCheckedModeBanner: false, // стандарт от Google
-        home: ProductInfo(),
-      );}
-      )
-    );*/
+import 'package:flutterapp/products/productList.dart';
 
 
-    /*return ChangeNotifierProvider<Cart>.value(
-        value: Cart(),
-        child: MaterialApp(
-          //debugShowCheckedModeBanner: false,  // стандарт от Google
-          home: ProductInfo(),
-            //ProductInfo()
-          ),
-        );*/
-  }
-}
 class ProductInfo extends StatefulWidget {
+
   String name,
       brand,
       picture;
   double price ,
       value,
       oldPrice;
-  int id;
+  int id, num;
   ProductInfo({ Key key,
     this.name, this.id, this.oldPrice, this.picture,
-    this.value, this.price, this.brand
+    this.value, this.price, this.brand, this.num = 0
 }):super(key:key);
   @override
   _ProductInfoState createState() => _ProductInfoState();
@@ -62,15 +22,15 @@ class ProductInfo extends StatefulWidget {
 
 class _ProductInfoState extends State<ProductInfo> {
 
+
   @override
   Widget build(BuildContext context) {
-    Cart bloc = Provider.of<Cart>(context);
-    /*int totalCount = 0;
-    if (bloc.cart.length > 0) {
-      totalCount = bloc.cart.values.reduce((a, b) => a + b);
-    }*/
-
+    int totalCount = 0;
+    cart.forEach((item){
+      totalCount = totalCount + item.num;
+    });
     return Scaffold(
+      key: Key(widget.num.toString()),
       appBar: AppBar(
         elevation: 0.1,
         backgroundColor: Colors.blue,
@@ -81,16 +41,43 @@ class _ProductInfoState extends State<ProductInfo> {
                 Icons.search,
                 color: Colors.white,
               ), onPressed: () {}),
-          new IconButton(
-              icon: Icon(
-                Icons.shopping_cart,
-                color: Colors.white,
+          Stack(
+
+            children: <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.shopping_cart,
+                  color: Colors.white,
+                ),
+                onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => ShoppingCart(cart,sum)))
+            ),
+              Positioned(
+                child: Stack(
+                children: <Widget>[
+                  Icon(Icons.brightness_1,
+                    size: 20.0, color: Colors.red[700]),
+                  Positioned(
+                    top: 3.0,
+                    right: 7,
+                    child: Center(
+                      child: Text('$totalCount',
+                      style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.w500),
+                      ),
+                    )
+                    ),
+                  ],
+                )
               ),
-              onPressed: () => Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => ShoppingCart()))
+              ]
           )
-        ],
-      ),
+            ]
+          ),
+
+
       body: ListView(
         children: <Widget>[
           Container(
@@ -102,31 +89,27 @@ class _ProductInfoState extends State<ProductInfo> {
               )),
             height: 300.0,
           ),
-         /* GestureDetector(
-              onTap: () {
-                bloc.addToCart(widget);
-              },
-              child: Container(
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    //image: AssetImage("assets/${index + 1}.jpg"),
-                    fit: BoxFit.fitWidth,
-                  ),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              )
-          ),*/
           RaisedButton(
             splashColor: Theme.of(context).primaryColor,
             highlightColor: Theme.of(context).primaryColor,
             color: Colors.white,
             child: Text('Добавить в корзину'),
             //style : TextStyle(fontWeight: FontWeight.bold, color: Theme.of(context).primaryColor, fontSize: 20),
-            onPressed: () {
+            onPressed: () {setState((){
+              if(cart.contains(widget)){
+                widget.num += 1;
+              }
+              else {
+                cart.add(widget); //update
+                widget.num = 1;
+              }
+              sum = 0;
+              cart.forEach((item){
+                sum = sum + item.num*item.price;
+
+              });});
                 //var bloc = Provider.of<Cart>(context, listen: false);
-                bloc.addToCart(widget);
+                //bloc.addToCart(widget);
             },
           )
         ],
--- lib/registration/shoppingCart.dart
+++ lib/registration/shoppingCart.dart
@@ -1,57 +1,83 @@
 import 'package:flutter/material.dart';
-import 'package:flutterapp/products/product.dart';
-import 'package:flutterapp/products/productInfo.dart';
-import 'package:provider/provider.dart';
-import 'package:flutterapp/registration/cart.dart';
-//import 'dart:convert';
 
-
-class ShoppingCart extends StatelessWidget{
-  ShoppingCart({Key key}):super(key:key);
-
-  Widget build(BuildContext context) {
-    var bloc = Provider.of<Cart>(context);
-    var cart = bloc.cart;
-    //var productList=json.decode(snapshot.data.toString());
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Корзина"),
-      ),
-      body: ListView.builder(
-        itemCount: cart.length,
-        itemBuilder: (context, index) {
-          ProductInfo giftIndex = cart.keys.toList()[index];
-          int count = cart[giftIndex];
-
-          return ListTile(
-            leading: Container(
-              height: 70,
-              width: 70,
-
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                 //image: Image.network(giftIndex.picture),
-                  fit: BoxFit.fitWidth,
-                ),
-                borderRadius: BorderRadius.circular(12),
-              ),
-            ),
-            //title: Text('Item Count: $count'),
-            title: Text('${giftIndex.name}'),
-            trailing: RaisedButton(
-              child: Text('Clear'),
-              color: Theme.of(context).buttonColor,
-              elevation: 1.0,
-              splashColor: Colors.blueGrey,
-              onPressed: () {
-                bloc.clear(giftIndex);
-              },
-            ),
-          );
-        },
-      ),
-    );
-  }
-
+class ShoppingCart extends StatefulWidget {
+  final cart;
+  double sum;
+  ShoppingCart(this.cart, this.sum);
+  @override
+  _ShoppingCart createState() => _ShoppingCart(cart, sum);
 }
 
+class _ShoppingCart extends State<ShoppingCart> {
+  @override
+  final cart;
+  double sum;
+  _ShoppingCart(this.cart, this.sum);
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar:AppBar(
+          title: Text('Корзина'),
+        ),
+        body: ListView(
+      //crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        ListView.separated(
+          itemBuilder: (context, index){
+            return /*Dismissible(
+              key: Key(cart[index].toString()),
+              onDismissed: (direction){
+                setState(() {
+
+
+                sum = sum - cart[index].price*cart[index].num;
+                cart.removeAt(index);
+                Scaffold.of(context).showSnackBar(SnackBar(content: Text("Продукт был удалён")));});
+              },
+              background: Container(
+                color: Colors.red,
+              ),
+              child:*/ ListTile(
+                title: Text(cart[index].name),
+                subtitle: Text(cart[index].num.toString()),
+                /*trailing: Text("\$${cart[index].price*cart[index].num}",
+                  style: TextStyle(color: Colors.redAccent, fontSize: 20, fontWeight: FontWeight.w500),),*/
+                trailing: IconButton(
+                  icon: Icon(
+                    Icons.delete ,
+                    color: Colors.red,
+                  ),
+                  onPressed:() {
+                    setState(() {
+                      sum = sum - cart[index].price*cart[index].num;
+                      cart.removeAt(index);
+                    });
+                    Scaffold.of(context).showSnackBar(SnackBar(content: Text("Продукт был удалён")));
+
+                    },
+                ),
+              );
+            //);
+          },
+          separatorBuilder: (context, index){
+            return Divider();
+          },
+          itemCount: cart.length,
+          shrinkWrap: true,
+        ),
+        Divider(),
+        Text("Total : \$$sum"),
+        RaisedButton(
+          splashColor: Theme.of(context).primaryColor,
+          highlightColor: Theme.of(context).primaryColor,
+          color: Colors.white,
+          child: Text('Оплатить'),
+          //style : TextStyle(fontWeight: FontWeight.bold, color: Theme.of(context).primaryColor, fontSize: 20),
+          onPressed: () {
+
+            },
+        )
+      ],
+    )
+    );
+  }
+}
