affa8dc95a968c4990fdd771c33046a4a41705e4
Pavivin <pavivin@yandex.ru>
2020-04-17 14:45:17 +0300

    Stripe payments

---
+++ lib/payments/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+
+class Payment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HomePage()
+    );
+  }
+}
