2acf85e835df44185f2bf452833b1d9f155e8d54
TagidickTagidick <49733518+TagidickTagidick@users.noreply.github.com>
2020-05-04 09:51:56 +0300

    Shopcart (#53)
    * .idea deleted
    
    * upd cart
    
    * scrollable cart
    
    * Auto stash before rebase of "origin/shopcart"
    
    not commit
    
    * debug totalcount
    
    * change a way to shopping cart anf delete unuseful file
    
    * Search
    
    * New shopping cart
    
    Co-authored-by: Alexey-Sennikov <nesnaiker@yandex.ru>
    Co-authored-by: Alexey-Sennikov <55105158+Alexey-Sennikov@users.noreply.github.com>

--- lib/registration/shoppingCart.dart
+++ lib/registration/shoppingCart.dart
@@ -1,88 +1,252 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutterapp/products/productList.dart';
-
 import '../payments/main.dart';
+
 class ShoppingCart extends StatefulWidget {
   final cart;
   ShoppingCart(this.cart);
   @override
-  _ShoppingCart createState() => _ShoppingCart(cart);
+  _ShoppingCartState createState() => _ShoppingCartState(cart);
 }
 
-class _ShoppingCart extends State<ShoppingCart> {
+class _ShoppingCartState extends State<ShoppingCart> {
   @override
   final cart;
-  _ShoppingCart(this.cart);
+  _ShoppingCartState(this.cart);
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar:AppBar(
-          title: Text('Корзина'),
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: Icon(
+            Icons.keyboard_arrow_left,
+            color: Colors.black,
+          ),
+          onPressed: () {},
         ),
-        body: ListView(
-      //crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        ListView.separated(
-          controller: ScrollController(),
-          itemCount: cart.length,
-          itemBuilder: (context, index){
-            return /*Dismissible(
-              key: Key(cart[index].toString()),
-              onDismissed: (direction){
-                setState(() {
-
-
-                sum = sum - cart[index].price*cart[index].num;
-                cart.removeAt(index);
-                Scaffold.of(context).showSnackBar(SnackBar(content: Text("Продукт был удалён")));});
-              },
-              background: Container(
-                color: Colors.red,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Корзина",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 21.0,
               ),
-              child:*/ ListTile(
-                title: Text(cart[index].name),
-                subtitle: Text(cart[index].num.toString()),
-                /*trailing: Text("\$${cart[index].price*cart[index].num}",
-                  style: TextStyle(color: Colors.redAccent, fontSize: 20, fontWeight: FontWeight.w500),),*/
-                trailing: IconButton(
-                  icon: Icon(
-                    Icons.delete ,
-                    color: Colors.red,
+            ),
+            ListView.separated(
+              controller: ScrollController(),
+              itemCount: cart.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                          width: 80.0,
+                          height: 80.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: Center(
+                              child: Container(
+                                width: 60.0,
+                                height: 60.0,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    fit: BoxFit.scaleDown,
+                                    image: NetworkImage(cart[index].picture),
+                                  ),
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                              )
+                          )
+                      ),
+                      SizedBox(width: 12.0),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Container(
+                                  width: 100.0,
+                                  child: Text(
+                                    cart[index].name,
+                                    style: TextStyle(fontWeight: FontWeight.bold,),
+                                  ),
+                                ),
+                                Spacer(),
+                                IconButton(
+                                  icon: Icon(
+                                    Icons.delete ,
+                                    color: Colors.red,
+                                  ),
+                                  onPressed:() {
+                                    setState(() {
+                                      sum = sum - cart[index].price*cart[index].num;
+                                      cart.removeAt(index);
+                                    });
+                                    Scaffold.of(context).showSnackBar(SnackBar(content: Text("Продукт был удалён")));
+                                  },
+                                ),
+                              ]
+                            ),
+                            SizedBox(height: 8.0),
+                            Row(
+                              children: <Widget>[
+                                Container(
+                                  width: 20.0,
+                                  height: 20.0,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[300],
+                                    borderRadius: BorderRadius.circular(1.0),
+                                  ),
+                                  child: IconButton(
+                                    icon: Icon(
+                                      Icons.add,
+                                      color: Colors.white,
+                                      size: 15.0,
+                                    ),
+                                    onPressed:() {},
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                                  child: Text(
+                                    cart[index].num.toString(),
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  width: 20.0,
+                                  height: 20.0,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[300],
+                                    borderRadius: BorderRadius.circular(1.0),
+                                  ),
+                                  child: IconButton(
+                                    icon: Icon(
+                                      Icons.add,
+                                      color: Colors.white,
+                                      size: 15.0,
+                                    ),
+                                    onPressed:() {},
+                                  ),
+                                ),
+                                Spacer(),
+                                Text(
+                                    cart[index].price.toString() + "р.",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    )
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-                  onPressed:() {
-                    setState(() {
-                      sum = sum - cart[index].price*cart[index].num;
-                      cart.removeAt(index);
-                    });
-                    Scaffold.of(context).showSnackBar(SnackBar(content: Text("Продукт был удалён")));
-
-                    },
+                );
+              },
+              separatorBuilder: (context, index){
+                return Divider();
+              },
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+            ),
+            SizedBox(height: 21.0),
+            Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Всего:",
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              );
-            //);
-          },
-          separatorBuilder: (context, index){
-            return Divider();
-          },
-          shrinkWrap: true,
-          scrollDirection: Axis.vertical,
+                Text(
+                  sum.toString() + "р.",
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 4.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Оплата за доставку",
+                  style: TextStyle(
+                    fontSize: 14.0,
+                  ),
+                ),
+                Text(
+                  "0р.",
+                  style: TextStyle(
+                    fontSize: 14.0,
+                  ),
+                ),
+              ],
+            ),
+            Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Итог:",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  sum.toString() + "р.",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Spacer(),
+            MaterialButton(
+              onPressed: () => Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => Payment())),
+              color: Colors.green,
+              height: 50.0,
+              minWidth: double.infinity,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Text(
+                "Оформить заказ",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 18.0),
+          ],
         ),
-        Divider(),
-        Text("Total : \$$sum"),
-        RaisedButton(
-          splashColor: Theme.of(context).primaryColor,
-          highlightColor: Theme.of(context).primaryColor,
-          color: Colors.white,
-
-          child: Text('Оплатить',
-              style: TextStyle(fontWeight: FontWeight.bold, color: Theme.of(context).primaryColor, fontSize: 20)),
-
-          onPressed: () => Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => Payment()))
-        )
-      ],
-    )
+      ),
     );
   }
-
 }
